e97ece3c5ae0f948a13a7e2a099380db3b7b216f
Uriel <urielbenitez96@hotmail.com>
2020-09-06 22:53:44 -0500

    menu js

---
+++ menu.js
@@ -0,0 +1,15 @@
+function Mostrar(){
+    document.getElementById('MenuDesplegable').style.display="block"
+}
+function Ocultar(){
+    document.getElementById('MenuDesplegable').style.display="none"
+}
+function desplegar(){
+   const valor=document.getElementById('MenuDesplegable')
+   if(valor.style.display=="none")
+   {Mostrar();
+   }
+   else{
+       Ocultar();
+   }
+}
