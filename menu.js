826eb34a8306fd5beaf842e5c48487b82a5093d4
Uriel <urielbenitez96@hotmail.com>
2020-09-11 17:10:04 -0500

    Actualización de menú hamburguesa

--- menu.js
+++ menu.js
@@ -1,16 +1,14 @@
-function Mostrar(){
-    document.getElementById('MenuDesplegable').style.display='block'
-}
-function Ocultar(){
-    document.getElementById('MenuDesplegable').style.display='none'
-}
-function desplegar(){
-   const valor=document.getElementById('MenuDesplegable')
-   if(valor.style.display=='none')
-   {
-       Mostrar();
-   }
-   else{
-       Ocultar();
-   }
+var desplegar=0;
+
+const Mostrar_Ocultar=() =>{
+
+    if (desplegar==0) {
+        const action=document.getElementById('MenuDesplegable').style.display="block"
+        console.log('felicidades tu menú se ha desplegado');
+        desplegar=1;
+    } else {
+        const action=document.getElementById('MenuDesplegable').style.display="none"
+        console.log('felicidades tu menú se ha ocultado');
+        desplegar=0;
+    }
 }
